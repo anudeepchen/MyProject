5ba865f515c47e70d74cdc4bb632b2c5120a1305
anudeepchen <anudeeprockz@gmail.com>
2016-05-21 10:59:49 -0700

    y

---
+++ static/static-only/static-only/static-only/static-only/js/shrink.js
@@ -0,0 +1,13 @@
+
+   
+document.getElementById("foodbutton").onclick = function () {
+        location.href = "/accounts/student_register";
+};
+
+  
+
+ 
+   
+document.getElementById("cookbutton").onclick = function () {
+        location.href = "/accounts/student_register";
+};
